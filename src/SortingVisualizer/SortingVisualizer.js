b1495cdd5952b28b12c492e815f42fa3f4e8e593
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-12-15 11:46:07 +0100

    display values in a bar format

---
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./SortingVisualizer.css";
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 200; i++) {
+            array.push(randomIntFromInterval(5, 750));
+        }
+        this.setState({array});
+    }
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div className="array-bar" key={idx} style={{height: `${value}px`}}></div>
+                ))}
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.resetArray()}>Quick Sort</button>
+                <button onClick={() => this.resetArray()}>Heap Sort</button>
+                <button onClick={() => this.resetArray()}>Bubble Sort</button>
+            </div>
+        );
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
