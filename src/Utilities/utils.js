b18a5cc326403563ae31a493f24a7c282c01ddd5
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2024-01-05 11:52:37 +0100

    new colors

--- src/Utilities/utils.js
+++ src/Utilities/utils.js
@@ -11,12 +11,12 @@ export function MakeDelay(ms) {
 }
 
 export var delay = 1
-export const PRIMARY_COLOR = 'rgb(113, 165, 255)';
-export const FINAL_COLOR = 'rgb(15, 245, 70)';
-export const POSITION_FINAL_COLOR = 'rgb(183, 51, 245)';
-export const COMPARE_COLOR = 'blue';
-export const SWAP_COLOR = 'red';
-export const MIN_COLOR = 'rgb(255, 112, 31)'
+export const PRIMARY_COLOR = 'rgb(88, 170, 255)';
+export const FINAL_COLOR = 'rgb(84, 232, 160)';
+export const POSITION_FINAL_COLOR = 'rgb(188, 111, 255)';
+export const COMPARE_COLOR = 'rgb(0, 128, 255)';
+export const SWAP_COLOR = 'rgb(255, 69, 69)';
+export const MIN_COLOR = 'rgb(255, 112, 31)';
 
 export function randomNumberFrom(l, r) {
     return Math.floor(Math.random() * (l - r + 1) + r)
